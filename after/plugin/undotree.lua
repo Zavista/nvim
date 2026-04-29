d3e011f3b5bd08afe1fbb9a77b535334d19462c3
Zavista <84294353+Zavista@users.noreply.github.com>
2026-04-29 01:47:26 -0400

    Some undo tree configurations

--- after/plugin/undotree.lua
+++ after/plugin/undotree.lua
@@ -1,3 +1,8 @@
-vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
+vim.g.undotree_WindowLayout = 2
+vim.g.undotree_SetFocusWhenToggle = 1
+vim.g.undotree_SplitWidth = 30
 vim.opt.undofile = true
 
+
+
+vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
--- lua/david/set.lua
+++ lua/david/set.lua
@@ -39,4 +39,4 @@ vim.opt.updatetime = 50
 vim.opt.colorcolumn = "80"
 
 vim.opt.splitright = true
-vim.opt.splitbelow = true
+
